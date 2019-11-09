58f0f010c85ede3829b9c819cfb304d7f36be681
theepag <theepag@live.com>
2019-11-09 23:55:56 +0530

    Farms Popup View is working Farms Popup update also fucking working

--- app/src/main/java/com/example/dairycattle/Home.java
+++ app/src/main/java/com/example/dairycattle/Home.java
@@ -1,16 +1,21 @@
 package com.example.dairycattle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -41,6 +46,11 @@ public class Home extends AppCompatActivity {
         btnLogout = (Button) findViewById(R.id.logout);
         buttonAddFarm = (Button)findViewById(R.id.addFarm);
 
+        ListViewFarms = (ListView) findViewById(R.id.listViewFarms);
+
+        farmList=new ArrayList<>();
+
+
 
 
         buttonAddFarm.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +58,30 @@ public class Home extends AppCompatActivity {
             public void onClick(View v) {
                 Intent inToAddFarm = new Intent(Home.this,addFarm.class);
                 startActivity(inToAddFarm);
+            }
+        });
+
+        ListViewFarms.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+                Farm farm = farmList.get(i);
+                updateFarmDetail(farm.getFarmName(),farm.getFarmRegNo(),farm.getFarmOwnName(),farm.getFarmVetDiv(),farm.getFarmAddress(),farm.getFarmContactNo(),farm.getFarmCattleCount(),farm.getFarmDairyCattleCount());
+
+
+                return true;
+            }
+        });
+
+
+        ListViewFarms.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+
+                Farm farm = farmList.get(i);
+                showFarmDetail(farm.getFarmName(),farm.getFarmRegNo(),farm.getFarmOwnName(),farm.getFarmVetDiv(),farm.getFarmAddress(),farm.getFarmContactNo(),farm.getFarmCattleCount(),farm.getFarmDairyCattleCount());
+
+
+
             }
         });
 
@@ -66,12 +100,6 @@ public class Home extends AppCompatActivity {
 
 
         });
-
-
-        ListViewFarms = (ListView) findViewById(R.id.listViewFarms);
-
-        farmList=new ArrayList<>();
-
 
     }
 
@@ -107,6 +135,100 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+
+
+
+
+    private void showFarmDetail(String Name, String RegNo, String OwnName, String VetDiv , String Address, String ContactNo, String CattleCount, String DairyCattleCount){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.viewfarms,null);
+
+        dialogBuilder.setView(dialogView);
+
+        final TextView editTextName = (TextView) dialogView.findViewById(R.id.FarmName);
+        final TextView editTextRegNo = (TextView) dialogView.findViewById(R.id.farmRegNo);
+        final TextView editTextOwnName = (TextView) dialogView.findViewById(R.id.farmOwnName);
+        final TextView editTextVetDiv = (TextView) dialogView.findViewById(R.id.farmVetDiv);
+        final TextView editTextAddress = (TextView) dialogView.findViewById(R.id.farmAddress);
+        final TextView editTextContactNo = (TextView) dialogView.findViewById(R.id.farmContactNo);
+        final TextView editTextCattleCount = (TextView) dialogView.findViewById(R.id.farmCattleCount);
+        final TextView editTextDairyCattleCount = (TextView) dialogView.findViewById(R.id.farmDairyCattleCount);
+
+        editTextName.setText(Name);
+        editTextRegNo.setText(RegNo);
+        editTextOwnName.setText(OwnName);
+        editTextVetDiv.setText(VetDiv);
+        editTextAddress.setText(Address);
+        editTextCattleCount.setText(CattleCount);
+        editTextDairyCattleCount.setText(DairyCattleCount);
+        editTextContactNo.setText(ContactNo);
+
+
+        dialogBuilder.setTitle("Showing Farm Details");
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+
+
+    }
+
+    private void updateFarmDetail(String Name, String RegNo, String OwnName, String VetDiv , String Address, String ContactNo, String CattleCount, String DairyCattleCount){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        final View dialogView = inflater.inflate(R.layout.update_farms,null);
+
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+        final EditText editTextRegNo = (EditText) dialogView.findViewById(R.id.editTextRegNo);
+        final EditText editTextOwnName = (EditText) dialogView.findViewById(R.id.editTextOwnName);
+        final EditText editTextVetDiv = (EditText) dialogView.findViewById(R.id.editTextVetDiv);
+        final EditText editTextAddress = (EditText) dialogView.findViewById(R.id.editTextAddress);
+        final EditText editTextContactNo = (EditText) dialogView.findViewById(R.id.editTextContactNo);
+        final EditText editTextCattleCount = (EditText) dialogView.findViewById(R.id.editTextCattleCount);
+        final EditText editTextDairyCattleCount = (EditText) dialogView.findViewById(R.id.editTextDairyCattleCount);
+        final Button btnCancell = (Button)dialogView.findViewById(R.id.buttonCancel);
+
+        editTextName.setText(Name);
+        editTextRegNo.setText(RegNo);
+        editTextOwnName.setText(OwnName);
+        editTextVetDiv.setText(VetDiv);
+        editTextAddress.setText(Address);
+        editTextCattleCount.setText(CattleCount);
+        editTextDairyCattleCount.setText(DairyCattleCount);
+        editTextContactNo.setText(ContactNo);
+
+        btnCancell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intToHome = new Intent(Home.this, Home.class);
+                startActivity(intToHome);
+
+
+            }
+
+
+        });
+
+
+        dialogBuilder.setTitle("Update Farm Details");
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+
+
+    }
+
+
+
+
+
 
 
 }
