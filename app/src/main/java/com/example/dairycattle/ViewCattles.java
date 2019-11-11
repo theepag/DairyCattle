67317109e20abda983c445047b2aa7b2958234d5
theepag <theepag@live.com>
2019-11-12 00:40:35 +0530

    getting into cattle

---
+++ app/src/main/java/com/example/dairycattle/ViewCattles.java
@@ -0,0 +1,75 @@
+package com.example.dairycattle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewCattles extends AppCompatActivity {
+
+    DatabaseReference databaseCattle;
+    ListView ListViewCattle;
+    List<Cattle> cattleList;
+    Button btnAddCattle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cattles);
+
+        btnAddCattle = (Button)findViewById(R.id.buttonAddCattle);
+
+        databaseCattle= FirebaseDatabase.getInstance().getReference("cattle");
+
+
+        ListViewCattle= (ListView) findViewById(R.id.ListViewCattle);
+
+        cattleList=new ArrayList<>();
+
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseCattle.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+
+                cattleList.clear();
+
+                for(
+                        DataSnapshot cattleSnapshot :dataSnapshot.getChildren())
+
+                {
+                    //getting farm
+                    Cattle cattle = cattleSnapshot.getValue(Cattle.class);
+                    //adding farm to the list
+                    cattleList.add(cattle);
+                }
+
+                CattleList adapters = new CattleList(ViewCattles.this, cattleList);
+                ListViewCattle.setAdapter(adapters);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
